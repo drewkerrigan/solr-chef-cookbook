ee8bf11e23e50e493a0ed06923dcd8c3e90e3709
drewkerrigan <dkerrigan@basho.com>
2013-04-16 16:39:05 -0400

    first commit

---
+++ recipes/default.rb
@@ -0,0 +1,14 @@
+# download a binary release
+remote_file "/tmp/solr-4.2.1.tgz" do
+  source "http://www.mirrorservice.org/sites/ftp.apache.org/lucene/solr/4.2.1/solr-4.2.1.tgz"
+end
+
+execute "extract" do
+  command "tar zxf solr-3.1.0.tgz"
+  cwd "/tmp"
+end
+
+execute "run_solr" do
+  command "java -jar start.jar"
+  cwd "/tmp/solr-4.2.1/example"
+end
