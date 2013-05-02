57ecb6234258b4ed85b12b2b993229d55eb36070
drewkerrigan <dkerrigan@basho.com>
2013-05-02 16:59:10 -0400

    adding support for address specification

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,8 @@
-default[:solr][:version] = "4.2.1"
-default[:solr][:dir] = "/opt/solr"
-default[:solr][:schema] = {
+default["solr"]["bind_address"] = node['ipaddress']
+default["solr"]["bind_port"] = "8983"
+default["solr"]["version"] = "4.2.1"
+default["solr"]["dir"] = "/opt/solr"
+default["solr"]["schema"] = {
 	"id" => {
 		"type" => "string", 
 		"indexed" => "true",
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,31 +1,31 @@
-directory node[:solr][:dir] do
+directory node["solr"]["dir"] do
   owner "root"
   mode "0755"
   action :create
 end
 
 template "schema.xml" do
-  path "#{node[:solr][:dir]}/schema.xml"
+  path "#{node["solr"]["dir"]}/schema.xml"
   source "schema.xml.erb"
   owner "root"
   group "root"
   mode "0644"
 end
 
-remote_file "#{Chef::Config[:file_cache_path]}/solr-#{node[:solr][:version]}.tgz" do
-  source "http://data.riakcs.net:8080/solr/solr-#{node[:solr][:version]}.tgz"
+remote_file "#{Chef::Config[:file_cache_path]}/solr-#{node["solr"]["version"]}.tgz" do
+  source "http://data.riakcs.net:8080/solr/solr-#{node["solr"]["version"]}.tgz"
   action :create_if_missing
 end
 
 bash "install_solr" do
   cwd Chef::Config[:file_cache_path]
   code <<-EOH
-    tar zxf solr-#{node[:solr][:version]}.tgz
-    sudo cp -R solr-#{node[:solr][:version]}/* #{node[:solr][:dir]}/
-    sudo rm #{node[:solr][:dir]}/example/solr/collection1/conf/schema.xml
-    sudo cp #{node[:solr][:dir]}/schema.xml #{node[:solr][:dir]}/example/solr/collection1/conf/schema.xml
+    tar zxf solr-#{node["solr"]["version"]}.tgz
+    sudo cp -R solr-#{node["solr"]["version"]}/* #{node["solr"]["dir"]}/
+    sudo rm #{node["solr"]["dir"]}/example/solr/collection1/conf/schema.xml
+    sudo cp #{node["solr"]["dir"]}/schema.xml #{node["solr"]["dir"]}/example/solr/collection1/conf/schema.xml
   EOH
-  creates "#{node[:solr][:dir]}/example/start.jar"
+  creates "#{node["solr"]["dir"]}/example/start.jar"
 end
 
 service "solr" do
